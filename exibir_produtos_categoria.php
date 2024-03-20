5f1d95f2bbbd7a10751b89e47e1e22f96abfce73
gleisonbt <brittobaptista93@gmail.com>
2024-03-20 19:23:36 -0300

    exibir produtos

--- exibir_produtos_categoria.php
+++ exibir_produtos_categoria.php
@@ -1,5 +1,29 @@
-<?php
-    #SELECT produtos.id, produtos.nome, categorias.nome as "nome_categoria"
-	#FROM produtos INNER JOIN categorias
-	#WHERE produtos.categoria_id = categorias.id;
-?>
+<html>
+	<head>
+		<title>Exibir Produtos</title>
+	</head>
+	<body>
+		<table border="2">
+			<tr>
+				<th>id</th>
+				<th>nome</th>
+				<th>categoria</th>
+			</tr>
+			<?php
+				$pdo = new PDO("sqlite:estoque.db");
+            	$queryProdutos = "SELECT produtos.id, produtos.nome, categorias.nome as \"nome_categoria\"
+				FROM produtos INNER JOIN categorias
+				WHERE produtos.categoria_id = categorias.id";
+            	$produtos = $pdo->query($queryProdutos);
+
+				foreach($produtos as $produto){
+					echo "<tr><td>".$produto["id"]."</td>";
+					echo "<td>".$produto["nome"]."</td>";
+					echo "<td>".$produto["nome_categoria"]."</td></tr>";
+				}
+	
+
+			?>
+		</table>
+	</body>
+</html>
