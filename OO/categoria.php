4f86b08522121c213adc59e105347543afc7bd1e
gleisonbt <brittobaptista93@gmail.com>
2024-03-20 19:54:38 -0300

    add class categoria

---
+++ OO/categoria.php
@@ -0,0 +1,27 @@
+<?php 
+    class Categoria{
+        private $id;
+        private $nome;
+
+        function __construct($id, $nome) {
+            $this->id = $id; 
+            $this->nome = $nome;
+        }
+
+        public function getId() { 
+            return $this->id;
+        }                   
+
+        public function setId($id) {
+            $this->id = $id;
+        }
+
+        public function getNome() {
+            return $this->nome;
+        }
+
+        public function setNome($nome) {
+            $this->nome = $nome;
+        }
+    }
+?>
